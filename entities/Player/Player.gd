8e2938990d4f6a8dd218818e49b6f74b06a496b2
MBoittin <mederic18@gmail.com>
2019-10-05 12:20:27 +0200

    Player: add player movement

--- entities/Player/Player.gd
+++ entities/Player/Player.gd
@@ -1,11 +1,23 @@
 extends KinematicBody2D
 
-var mousePosition
+var mouse_position
+var movement
 
 func _ready():
 	pass
 
 func _process(delta):
-	mousePosition = get_local_mouse_position()
-	rotation += mousePosition.angle()*0.1
+	mouse_position = get_local_mouse_position()
+	rotation += mouse_position.angle()*0.1
+	movement = Vector2(0, 0)
+	if (Input.is_action_pressed("ui_up")):
+		movement += Vector2(0, -1)
+	if (Input.is_action_pressed("ui_down")):
+		movement += Vector2(0, 1)
+	if (Input.is_action_pressed("ui_right")):
+		movement += Vector2(1, 0)
+	if (Input.is_action_pressed("ui_left")):
+		movement += Vector2(-1, 0)
+	movement.normalized()
+	move_and_collide(movement * 2)
 	pass
