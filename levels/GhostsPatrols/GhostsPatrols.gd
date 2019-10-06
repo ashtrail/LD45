bc756f8d2355a0cead21501115499294702abfac
Simon Andrieux <simon.andrieux@efrei.net>
2019-10-06 10:04:10 +0200

    ghost patrol as node of a path for better useability

--- levels/GhostsPatrols/GhostsPatrols.gd
+++ levels/GhostsPatrols/GhostsPatrols.gd
@@ -1,18 +1,11 @@
-extends Node2D
+extends PathFollow2D
 
-var paths = []
 export var SPEED = 200
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	for node1 in get_children():
-		if node1 is Path2D:
-			for node2 in node1.get_children():
-				if node2 is PathFollow2D:
-					paths.append(node2)
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	for path in paths:
-		var ghost = path.get_children()[0]
-		if !ghost.is_triggered():
-			path.set_offset(path.get_offset() + SPEED*delta)
+	if !$Ghost.is_triggered():
+		set_offset(get_offset() + SPEED*delta)
