1c598589171a438deb847914021e50830145d1a1
Simon Andrieux <simon.andrieux@efrei.net>
2019-10-07 12:38:40 +0200

    queue system  in narator

--- Narator.gd
+++ Narator.gd
@@ -1,36 +1,59 @@
 extends RichTextLabel
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-var newText = ""
 var textIndex = 0
 var timer = 0
 
+var queue: Array = []
+var current_text: String
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	bbcode_text = "BEBE TES OU"
-	bbcode_text = "[center]" + bbcode_text
+	current_text = "BEBE TES OU"
+	bbcode_text = "[center]" + current_text
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if textIndex < newText.length():
+	
+	if textIndex < current_text.length() && $Timer.is_stopped():
 		timer += 1
 		if timer % 10 == 0:
-			bbcode_text += newText[textIndex]
+			bbcode_text += current_text[textIndex]
 			textIndex += 1
+		return
+	
+	if textIndex == current_text.length():
+		if $Timer.is_stopped():
+			$Timer.start()
+
 	pass
 
 
 func _appendText(var appened_text: String, var path_to_audio: String = ""):
-	newText = appened_text
-	textIndex = 0
-	bbcode_text = "[center]"
+	if current_text.empty():
+		bbcode_text = "[center]"
+		current_text = appened_text
+	else:
+		queue.append(appened_text)
 	if !path_to_audio.empty():
 		var sfx = load(path_to_audio) 
 		sfx.set_loop(false)
 		$GodSpeaker.stop()
 		$GodSpeaker.set_stream(sfx)
 		$GodSpeaker.play()
+	
+
+
+func _on_Timer_timeout():
+	$Timer.stop()
+	textIndex = 0
+	if queue.size() > 0:
+		current_text = queue.front()
+		bbcode_text = "[center]"
+		
+		queue.pop_front()
+	else:
+		current_text = ""
+		bbcode_text = "[center]"
+		
 	
